2726f14b5b2c5f0f4c0cd7472726bce7004f8a39
Anton Tsyrkunov <antontsyrkunov@gmail.com>
2023-06-15 00:26:10 +0300

    edited API

--- src/API/API.js
+++ src/API/API.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL = "https://www.googleapis.com/books/v1";
 const API_KEY = "AIzaSyD7siH3qgQrWd6jVuDj43il8r36NIvUmEI";
 
-const getBooks = (query) => {
+const getBooks = () => {
     const response = axios.get(`${BASE_URL}/volumes?key=${API_KEY}`);
     return response;
 };
